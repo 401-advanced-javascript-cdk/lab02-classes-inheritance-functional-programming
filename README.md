3aeb9b9519897139c9eb367ce4e1cde4a1cd1c72
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-14 20:31:45 -0700

    add files from lab branch

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# lab02-classes-inheritance-functional-programming
+![CF](http://i.imgur.com/7v5ASc8.png) LAB
+=================================================
+
+
+## Lab 02 - Classes, Inheritance, and Functional Programming
+
+### Author: Chris Kozlowski
+
+### Links and Resources
+* [Submission PR](https://github.com/401-advanced-javascript-cdk/lab02-classes-inheritance-functional-programming/pull/1)
+* [Travis](https://travis-ci.com/401-advanced-javascript-cdk/lab02-classes-inheritance-functional-programming)
+
+### Modules
+#### `objects/vehicle-class.js`
+##### Exported Values and Methods
+###### `Car(name) -> `object`
+Returns a car object.  Created with a Class constructor that extends from the Vehicle class.
+###### `Motocycle(name) -> `object`
+Returns a motorcycle object.  Created with a Class constructor that extends from the Vehicle class.
+###### `drive() -> `string`
+###### `stop() -> `string`
+Class methods inherited from the Vehicle class.  Returns a response string.
+###### `wheelie() -> `string`
+Class method from Motorcycle Class constructor which returns a response string.
+
+#### `objects/vehicle-factory.js`
+##### Exported Values and Methods
+###### `CarFactory(name)` -> `object`
+Returns a car object created from a factory function.
+###### `MotorcycleFactory(name)` -> `object`
+Returns a motorcycle object created from a factory function.
+###### `drive() -> `string`
+###### `stop() -> `string`
+Methods inherited from the Vehicle function.  Returns a response string.
+###### `wheelie() -> `string`
+Method from MotorcycleFactory which returns a response string.
+
+#### Tests
+* `npm test`
+* objects
+  * Cars have 4 wheels
+  * Bikes have 2 wheels
+  * Bikes can wheelie
+  * All vehicles can drive and stop
+* list
+  * Creates an empty list when initially called
+  * Has a properly tracked length value
+  * Can perform the push method and checks that an argument is present
+  * Can perform the unshift method and checks that an argument is present
+  * Can pop an item off the end of the data set
+  * Can shift an item off the front of the data set
+  * Can apply a callback with forEach
+  * Checks that the callback is a function
---
+++ list/__tests__/list.test.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// These 2 should be interchangeable!
+const List = require('../list.js');
+// const List = require('../list-constructor.js');
+
+const prepList = () => {
+  let stuff = new List();
+  stuff.push('a');
+  stuff.push('b');
+  stuff.push('c');
+  return stuff;
+};
+
+describe('List Data Structure', () => {
+
+  it('starts with a length of 0 and an empty data set', () => {
+    let stuff = new List();
+    expect(stuff.length).toEqual(0);
+    expect(stuff.data).toEqual({});
+  });
+
+  it('pushes items to the end of the data set', () => {
+    let list = prepList();
+    expect(list.length).toEqual(3);
+    expect(list.data[1]).toEqual('b');
+  });
+
+  it ('throws an error if push is called without an argument', () => {
+    let list = prepList();
+    expect(() => list.push()).toThrow();
+  });
+
+  it('shifts 1 item off the front of the data set', () => {
+    let list = prepList();
+    let shifted = list.shift();
+    expect(list.length).toEqual(2);
+    expect(list.data[0]).toEqual('b');
+    expect(shifted).toEqual('a');
+  });
+
+  it('unshifts an item to the front of the data set', () => {
+    let list = prepList();
+    expect(list.length).toEqual(3);
+    expect(list.data[0]).toEqual('a');
+    list.unshift('to the front');
+    expect(list.length).toEqual(4);
+    expect(list.data[0]).toEqual('to the front');
+  });
+
+  it('throws an error if unshift is called without an argument', () => {
+    let list = prepList();
+    expect(() => list.unshift()).toThrow();
+  });
+
+  it('Can apply a callback with forEach', () => {
+    let list = prepList();
+    list.forEach(value => {
+      value = 'altered by callback';
+      return value;
+    });
+    expect(list.data[0]).toEqual('altered by callback');
+    expect(list.data[1]).toEqual('altered by callback');
+    expect(list.data[2]).toEqual('altered by callback');
+  });
+
+  it('throws an error if forEach is called without a callback', () => {
+    let list = prepList();
+    expect(() => list.unshift()).toThrow();
+  });
+
+  it('throws an error in forEach if called with any argument that is not a function', () => {
+    let list = prepList();
+    let nonFuncs = [100, 'not a function', {this: 'fail'}, [1,2,3], true];
+    nonFuncs.forEach(arg => {
+      expect(() => list.forEach(arg)).toThrow();
+    });
+  });
+});
